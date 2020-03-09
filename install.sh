d8ec1e263f940eb94211b7970b83817fce9d5237
webermaster <cpu.kmw@gmail.com>
2020-03-09 15:06:28 -0500

    changed cask

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ mkdir -p $BREWDIR && curl -L https://github.com/Homebrew/brew/tarball/master | t
 
 #install brew cask
 BREW=~/Developer/homebrew/bin/brew
-$BREW tap caskroom/cask
+$BREW tap homebrew/cask-cask
 
 #install kegs
 $BREW install vim

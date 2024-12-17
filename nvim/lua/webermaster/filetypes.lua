f47c0b4d2b1cc400883447113ac42d9a661c5938
Webermaster <cpu.kmw@gmail.com>
2024-12-16 22:24:59 -0600

    added after

--- nvim/lua/webermaster/filetypes.lua
+++ nvim/lua/webermaster/filetypes.lua
@@ -6,52 +6,6 @@ local autocmd = vim.api.nvim_create_autocmd
 cmd('syntax on')                          -- Turn syntax highlighting on
 cmd('filetype plugin indent on')          -- file detection
 
--- Set tab settings for specific file types
-augroup('yaml', {
-    clear = false
-})
-autocmd({'FileType'}, {
-    group = 'yaml',
-    pattern = {'yaml', 'yml'},
-    command = 'setlocal tabstop=2 softtabstop=2 shiftwidth=2'
-})
-
-augroup('xml', {
-    clear = false
-})
-autocmd({'FileType'}, {
-    group = 'xml',
-    pattern = {'xml'},
-    command = 'setlocal tabstop=2 softtabstop=2 shiftwidth=2'
-})
-
-augroup('json', {
-    clear = false
-})
-autocmd({'FileType'}, {
-    group = 'json',
-    pattern = {'json'},
-    command = 'setlocal tabstop=2 softtabstop=2 shiftwidth=2'
-})
-
-augroup('tf', {
-    clear = false
-})
-autocmd({'FileType'}, {
-    group = 'tf',
-    pattern = {'tf'},
-    command = 'setlocal tabstop=2 softtabstop=2 shiftwidth=2'
-})
-
-augroup('lua', {
-    clear = false
-})
-autocmd({'FileType'}, {
-    group = 'lua',
-    pattern = {'lua'},
-    command = 'setlocal tabstop=2 softtabstop=2 shiftwidth=2'
-})
-
 augroup('cfn_template', {
     clear = false
 })
--- nvim/lua/webermaster/lazy/telescope.lua
+++ nvim/lua/webermaster/lazy/telescope.lua
@@ -1,17 +1,17 @@
 return {
   {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
         require('telescope').setup({
-          file_ignore_patterns = { 
-            ".venv" 
+          file_ignore_patterns = {
+            ".venv"
           }
         })
 
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+        vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>pf', builtin.git_files, {})
         vim.keymap.set('n', '<leader>pws', function()
           local word = vim.fn.expand("<cword>")
           builtin.grep_string({ search = word })

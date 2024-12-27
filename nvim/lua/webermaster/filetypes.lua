567aaae5424252e1152dda08bc98275d16fa8781
Webermaster <cpu.kmw@gmail.com>
2024-12-26 20:29:55 -0600

    added back cfn template filetype

--- nvim/lua/webermaster/filetypes.lua
+++ nvim/lua/webermaster/filetypes.lua
@@ -6,3 +6,16 @@ local autocmd = vim.api.nvim_create_autocmd
 cmd('syntax on')                          -- Turn syntax highlighting on
 cmd('filetype plugin indent on')          -- file detection
 
+augroup('cfn_template', {
+    clear = false
+})
+autocmd({'BufNewFile','BufFilePre','BufRead'}, {
+    group = 'cfn_template',
+    pattern = {'*.template'},
+    command = 'set filetype=yaml'
+})
+autocmd({'FileType'}, {
+    group = 'cfn_template',
+    pattern = {'template'},
+    command = 'setlocal tabstop=2 softtabstop=2 shiftwidth=2'
+})
